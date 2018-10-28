40033aa401c5a83cb87d59bb92de306f34a043c9
Kevin Pradervand <kevin.pradervand@heig-vd.ch>
2018-10-28 21:31:33 +0100

    le client avance

---
+++ Client/js/app.js
@@ -0,0 +1,166 @@
+
+
+function getDevLvl(totalCommits){
+    return totalCommits/50;
+}
+
+function createStatChart (htmlID, labels, datas, devLvl){
+
+    let ctx = document.getElementById(htmlID).getContext("2d");
+    let myChart = new Chart(ctx, {
+        type: "horizontalBar",
+        data: {
+            labels: labels,
+            datasets: [{
+                label : "Niv "+devLvl,
+                //c'est moche mais pour commencer à zero ça marche le zéro tout seul sans barre
+                data: datas,
+                backgroundColor: [
+                    'rgba(255, 0, 0, 0.6)',
+                    'rgba(255, 0, 0, 0.6)',
+                    'rgba(255, 0, 0, 0.6)',
+                    'rgba(255, 0, 0, 0.6)',
+                    'rgba(255, 0, 0, 0.6)',
+                    'rgba(255, 0, 0, 0.6)'
+                ],
+                borderColor: [
+                    'rgba(255,255,255,1)',
+                    'rgba(255,255,255,1)',
+                    'rgba(255,255,255,1)',
+                    'rgba(255,255,255,1)',
+                    'rgba(255,255,255,1)',
+                    'rgba(255,255,255,1)'
+                ],
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontSize: 15,
+                    fontColor: 'white'   
+                }
+            },
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        color: 'rgba(171,171,171,1)',
+                        lineWidth: 1
+                    },
+                    ticks: {
+                        beginAtZero:true,
+                        fontColor: 'white'
+                    }
+                }],
+                xAxes:[{
+                    ticks: {
+                        fontColor: 'white',
+                    }
+                }] 
+            }
+            
+        }
+    });
+
+}
+
+function setAvatar(id,url){
+    let img = document.getElementById(id);
+    img.src = url;
+}
+
+function setDev (dev1, dev2){
+    
+    dev1.avatarurl = getUser('Nortalle').then(user => {return user.avatar_url});
+    
+
+    setAvatar('avatar1', dev1.avatarurl);
+    setAvatar('avatar2', dev2.avatarurl);
+}
+
+function updateWOFDisplay (wallOfFame){
+    document.getElementById('place1').innerHTML = wallOfFame.pl1;
+    document.getElementById('place2').innerHTML = wallOfFame.pl2;
+    document.getElementById('place3').innerHTML = wallOfFame.pl3;
+    document.getElementById('place4').innerHTML = wallOfFame.pl4;
+    document.getElementById('place5').innerHTML = wallOfFame.pl5;
+    document.getElementById('place6').innerHTML = wallOfFame.pl6;
+    document.getElementById('place7').innerHTML = wallOfFame.pl7;
+    document.getElementById('place8').innerHTML = wallOfFame.pl8;
+}
+
+function getStatTable (dev){
+    let table = [dev.live, dev.languageP1, dev.languageP2, dev.languageP3, dev.languageP4, dev.languageP5];
+    return table;
+}
+
+function getLanguageTable(dev){
+    let table = ["live",dev.languageT1 , dev.languageT2, dev.languageT3, dev.languageT4, dev.languageT5];
+    return table;
+}
+
+function fight(){
+    
+}
+
+let dev1 = {
+    name: "",
+    live: 0,
+    languageT1: "",
+    languageP1: 0,
+    languageT2: "",
+    languageP2: 0,
+    languageT3: "",
+    languageP3: 0,
+    languageT4: "",
+    languageP4: 0,
+    languageT5: "",
+    languageP5: 0,
+    avatarurl: ""
+};
+
+let dev2 = {
+    name: "",
+    live: 0,
+    languageT1: "",
+    languageP1: 0,
+    languageT2: "",
+    languageP2: 0,
+    languageT3: "",
+    languageP3: 0,
+    languageT4: "",
+    languageP4: 0,
+    languageT5: "",
+    languageP5: 0,
+    avatarurl: ""
+};
+
+//Attention aux Accents de merdes 
+let wallOfFame = {
+    pl1: "",
+    pl2: "",
+    pl3: "",
+    pl4: "",
+    pl5: "",
+    pl6: "",
+    pl7: "",
+    pl8: ""
+}
+
+//document.getElementById('fbutton').addEventListener("click", function(){setDev(dev1, dev2);}, false);
+
+updateWOFDisplay (wallOfFame);
+
+setDev(dev1,dev2);
+
+createStatChart("statChartDev1", getLanguageTable(dev1), getStatTable(dev1), getDevLvl(dev1.live));
+createStatChart("statChartDev2",  getLanguageTable(dev2),  getStatTable(dev2), getDevLvl(dev2.live));
+
+updateWOFDisplay (wallOfFame);
+
+document.getElementById('place1').innerHTML = getUser('Nortalle').then(user => {return user.json().avatar_url});
+
+
+
+
+
